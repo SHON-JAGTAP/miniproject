6bad7ab9bf92569d423fac16d6a8a911a31fdc5e
SHON-JAGTAP <jagtapshon10@gmail.com>
2026-02-03 20:30:54 +0530

    add a

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -3,18 +3,15 @@ const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-
-  ssl: {
-    rejectUnauthorized: false
-  },
-
+  ssl: "Amazon RDS",   
   waitForConnections: true,
-  connectionLimit: process.env.DB_CONNECTION_LIMIT || 10,
-  queueLimit: 0,
+  connectionLimit: 10,
 });
+
 (async () => {
   try {
     const conn = await pool.getConnection();
